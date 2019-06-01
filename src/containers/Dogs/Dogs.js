f0057bd6d18c688982b887d0ab1330180e35129a
Rodrigo Cortes <racortes2@uc.cl>
2019-06-01 18:47:17 -0400

    many dogs and cats

--- src/containers/Dogs/Dogs.js
+++ src/containers/Dogs/Dogs.js
@@ -1,32 +1,49 @@
 import React, { Component } from 'react';
 import './Dogs.css';
 import { getRandomDogsImages } from '../../wrapper/dogs';
+import { getRandomCatsImages } from '../../wrapper/cats';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      imageLoaded: null,
+      dogsImagesLoaded: null,
+      catsImagesCatLoaded: null,
     };
-    this.getDog(1);
+    this.getDog(5);
+    this.getCat(5);
   }
 
   getDog(quantity) {
     getRandomDogsImages(quantity).then(dogs => {
       this.setState({
-        imageLoaded: dogs.data.message[0],
+        dogsImagesLoaded: dogs.data.message,
+      });
+    });
+  }
+
+  getCat(quantity) {
+    getRandomCatsImages(quantity).then(cats => {
+      this.setState({
+        catsImagesCatLoaded: cats.data,
       });
     });
   }
 
   render() {
-    const { imageLoaded } = this.state;
-    if (imageLoaded !== null) {
+    const { dogsImagesLoaded, catsImagesCatLoaded } = this.state;
+    console.log(catsImagesCatLoaded);
+    if (dogsImagesLoaded !== null && catsImagesCatLoaded !== null) {
       return (
         <div className="Home">
           <div className="lander">
             <h1>Dogs And Cats</h1>
-            <img src={imageLoaded} />
+            {dogsImagesLoaded.map(dog => (
+              <img src={dog} />
+            ))}
+            {catsImagesCatLoaded.map(cat => (
+              <img src={cat.url} />
+            ))}
           </div>
         </div>
       );
