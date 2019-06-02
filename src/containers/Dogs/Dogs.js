c1ed83a1cd0c11526c345c79bab81a59e07d0da9
Rodrigo Cortes <racortes2@uc.cl>
2019-06-01 22:16:51 -0400

    render one image of dog and cat

--- src/containers/Dogs/Dogs.js
+++ src/containers/Dogs/Dogs.js
@@ -7,43 +7,58 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dogsImagesLoaded: null,
-      catsImagesCatLoaded: null,
+      imagesLoaded: null,
     };
     this.getDog(5);
-    this.getCat(5);
   }
 
   getDog(quantity) {
     getRandomDogsImages(quantity).then(dogs => {
-      this.setState({
-        dogsImagesLoaded: dogs.data.message,
-      });
+      const { imagesLoaded } = this.state;
+      if (imagesLoaded === null) {
+        this.setState({
+          imagesLoaded: dogs.data.message,
+        });
+      } else {
+        const newState = imagesLoaded.concat(dogs.data.message);
+        this.setState({
+          imagesLoaded: newState,
+        });
+      }
     });
   }
 
   getCat(quantity) {
     getRandomCatsImages(quantity).then(cats => {
+      const { imagesLoaded } = this.state;
+      const imagesCats = [];
+      cats.data.map(cat => imagesCats.push(cat.url));
+      const newState = imagesLoaded.concat(imagesCats);
       this.setState({
-        catsImagesCatLoaded: cats.data,
+        imagesLoaded: newState,
       });
+    });
+    this.getDog(5);
+  }
+
+  changeState() {
+    const { imagesLoaded } = this.state;
+    imagesLoaded.shift();
+    const newState = imagesLoaded;
+    this.setState({
+      imagesLoaded: newState,
     });
   }
 
   render() {
-    const { dogsImagesLoaded, catsImagesCatLoaded } = this.state;
-    console.log(catsImagesCatLoaded);
-    if (dogsImagesLoaded !== null && catsImagesCatLoaded !== null) {
+    const { imagesLoaded } = this.state;
+    if (imagesLoaded !== null) {
+      this.getCat(5);
       return (
         <div className="Home">
           <div className="lander">
             <h1>Dogs And Cats</h1>
-            {dogsImagesLoaded.map(dog => (
-              <img src={dog} />
-            ))}
-            {catsImagesCatLoaded.map(cat => (
-              <img src={cat.url} />
-            ))}
+            <img src={imagesLoaded[0]} onClick={() => this.changeState()} />
           </div>
         </div>
       );
